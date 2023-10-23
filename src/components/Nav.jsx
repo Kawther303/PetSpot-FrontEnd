e3c447d429352b4e02fd342ab186c24a505ed5b5
kawthar alaraibi <kwther@gmail.com>
2023-10-23 09:15:01 +0300

    show lists

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,8 @@ const Nav = ({ user, handleLogOut }) => {
     userOptions = (
       <nav>
         <h3>Welcome {user.email}!</h3>
-        <Link onClick={handleLogOut} to=" ">Log Out
+        <Link onClick={handleLogOut} to=" ">
+          Log Out
         </Link>
       </nav>
     )
@@ -18,14 +19,15 @@ const Nav = ({ user, handleLogOut }) => {
       <Link to="/register">Register</Link>
       <Link to="/signin">Sign In</Link>
       <Link to="/Update profile">Update</Link>
+      <Link to="/pets">Pets</Link>
+      <Link to="/petItems">Items</Link>
     </nav>
   )
 
   return (
     <header>
       <Link to="/">
-        <div className="logo-wrapper" alt="logo">
-        </div>
+        <div className="logo-wrapper" alt="logo"></div>
       </Link>
       {user ? userOptions : publicOptions}
     </header>
